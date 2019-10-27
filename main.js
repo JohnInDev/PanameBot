65dd8ecc0674f8d7e5aa40b1b1ef7896ad0559f2
JohnInDev <56977164+JohnInDev@users.noreply.github.com>
2019-10-27 20:47:47 +0100

    ajout des events

--- main.js
+++ main.js
@@ -2,11 +2,8 @@ const { Client } = require('discord.js');
 const { TOKEN, PREFIX } = require("./config");
 const client = new Client();
 
-client.on('ready', () => {
-  console.log("Je suis près");
-});
-
 client.on('message', msg => {
+
   if (msg.author.bot) return;
   const args = msg.content.split(/ +/g);
   const cmd = args.shift().toLowerCase();
@@ -14,6 +11,15 @@ client.on('message', msg => {
     msg.reply('Pong!');
   }
 }
+);
+client.on("guildMemberAdd", member => {
+  const channel = client.channels.find(r => r.name === "général");
+  channel.send(`${member} a rejoin la communauté PanameRP, n'oublie pas de prendre tes rôle`);
 });
 
 client.login(TOKEN);
+
+client.on("ready", () => console.log("Je suis près"));
+client.on("error", console.error);
+client.on("warn", console.warn);
+client.on("debug", console.log);
